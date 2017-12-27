82ec61902d86746cdf1d5c8e18d9e2313d12ea97
Magnus Lindhé <lindhee@gmail.com>
2017-12-27 16:38:53 +0100

    CNN: Can now read images into a tensor
    Added a toy directory with 10 cats and 10 dogs

--- CNN/cats_vs_dogs.py
+++ CNN/cats_vs_dogs.py
@@ -4,35 +4,66 @@
 # Intended for Python 2.7.
 # Magnus Lindhé, 2017
 
+import os
 import numpy as np
 import tensorflow as tf
-import os
 import matplotlib.pyplot as plt
 import math
 
+# Remove TensorFlow warnings about unused GPU instructions
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2' 
+
 # Path to images should be relative to current working directory.
 cwd = os.getcwd()
-imagePath = cwd + "/data/train/"
+imageFilter = cwd + "/data/small/*.jpg"
+print("File filter: " + imageFilter)
 
-# Read images and resize them to constant size.
-# NOTE: This now creates some distortion of images that have other proportions. So a better way would 
-# be to pad each image to the correct proportions, then resize it.
-fileName = imagePath + "cat.1000.jpg"
-print("Reading file " + fileName)
-imageFile = tf.read_file(fileName)
-image = tf.image.decode_jpeg(imageFile,1) # 1 = convert to grayscale
-resized_image = tf.image.resize_images(image, [200, 200])
+# The code for loading images is borrowed from 
+# "https://gist.github.com/eerwitt/518b0c9564e500b4b50f"
+# Make a queue of file names
+name_list = tf.train.match_filenames_once(imageFilter)
+filename_queue = tf.train.string_input_producer(name_list)
 
-# Start an interactive session
-init = tf.initialize_all_variables()
-sess = tf.Session()
-sess.run(init)
-im = sess.run(resized_image)
+# Each image is fetched by reading a whole file.
+image_reader = tf.WholeFileReader()
 
-# Display the image
-plt.figure()
-plt.imshow(np.reshape(im,(200,200)))
-plt.show()
+# Read a whole file from the queue, the first returned value in the tuple is the
+# filename which we are ignoring.
+_, image_file = image_reader.read(filename_queue)
+
+# Decode the image as a JPEG file, this will turn it into a Tensor which we can
+# then use in training.
+image = tf.image.decode_jpeg(image_file)
+
+# Start a new session to show example output.
+with tf.Session() as sess:
+    # Open a log file that we can later view by running
+    # "tensorboard --logdir=/tmp/log" on the command line.
+    writer = tf.summary.FileWriter("/tmp/log", sess.graph)  
+    
+    # Required to get the filename matching to run.
+    init_op = tf.local_variables_initializer()
+    sess.run(init_op)
+
+    # Coordinate the loading of image files.
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(coord=coord)
+
+    t = sess.run(image)
+    
+    # Plot the file
+    plt.figure()
+    plt.imshow(np.squeeze(t))
+    plt.show()
+
+    # Finish off the filename queue coordinator.
+    coord.request_stop()
+    coord.join(threads)
+    
+    writer.close()
+
+# resized_image = tf.image.resize_images(image, [200, 200])
+
 
 
 
