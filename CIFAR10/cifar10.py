b68e7d6f5e9ce850738664ddd20456d0098cbf8c
Magnus Lindhé <magnus.lindhe@electrolux.com>
2018-03-26 18:19:10 +0200

    CIFAR: Add trace printout

--- CIFAR10/cifar10.py
+++ CIFAR10/cifar10.py
@@ -385,6 +385,9 @@ def maybe_download_and_extract():
     os.makedirs(dest_directory)
   filename = DATA_URL.split('/')[-1]
   filepath = os.path.join(dest_directory, filename)
+  
+  print("Checking: " + str(filepath))
+  
   if not os.path.exists(filepath):
     def _progress(count, block_size, total_size):
       sys.stdout.write('\r>> Downloading %s %.1f%%' % (filename,
